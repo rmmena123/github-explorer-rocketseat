1ebef53b584e111144af8f4511747e4e1adc2ce5
Rodrigo Mena <rm.mena86@gmail.com>
2022-01-25 08:58:37 -0300

    Chapter I + Primeiro Componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,32 @@
+const repositoryName = "unform"; // Fazer interpolação - {repositoryName}
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositório</a>
+                </li>
+
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositório</a>
+                </li>
+
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+import { App } from "./App"
+// O que quero exibir em tela, dentro de qual elemento renderizar
+render(<App/>, document.getElementById("root"))
+
