0d9405ca4b5ab117f53f0aee9c164c6239cae1ce
Rodrigo Mena <rm.mena86@gmail.com>
2022-01-25 12:33:38 -0300

    Chapter I - Chamadas HTTP - Listando Repositórios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,11 @@
 export function RepositoryItem(props) {
     return(
         <li>
-            <strong>{props.repository?.name ?? "Default"}</strong>
-            <p>{props.repository?.description ?? "Default"}</p>
-            <a href="">{props.repository?.link ?? "Link"}</a>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+            <a href={props.repository.html_url}>
+                Acessar Repositório
+            </a>
         </li>
     );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,28 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
 import "../styles/repositories.scss"
 
-const repository = {
-    name: "unform",
-    description: "Forms in React",
-    link: "Acessar Repositório"
-}
-
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    // Sem o segundo parâmetro, entra em loop e executa inúmeras vezes
+    // Se usar o setRepositories, entra em loop também
+    useEffect(() => {
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []) 
+    // Dependência vazia - executa uma única vez quando o componente é renderizado na tela - imprime uma vez vazio e depois com os dados
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
             </ul>
         </section>
     )
