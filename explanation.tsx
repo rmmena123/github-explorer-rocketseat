ebba1f15670331b4e2828050d53fc98d7166f558
Rodrigo Mena <rm.mena86@gmail.com>
2022-01-25 17:34:54 -0300

    Chapter I - Usando Typescript - Fundamentos do Typescript 2

--- explanation.tsx
+++ explanation.tsx
@@ -1,6 +1,7 @@
 // Superset do Javascript - adiciona um leque de funcionalidades a ele
 // Tipagem dinâmica - permitir passar tipos de dados indesejados por parâmetro
 // Faz checagem de tipos enquanto estamos desenvolvendo - em produção roda com Javascript
+// Inferência de tipos - já consegue determinar os tipos de informações
 type User = {
     name: string
     email: string
@@ -14,7 +15,7 @@ function showWelcomeMessage(user: User) {
     return `Welcome ${user.name}, your e-mail is ${user.email}. Your city is ${user.address.city} and your state are ${user.address.state}`;
 }
 
-showWelcomeMessage({
+const message = showWelcomeMessage({
     name: "John Doe",
     email: "john@doe",
     address: {
