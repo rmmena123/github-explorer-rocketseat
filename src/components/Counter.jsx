b66a13c7a67fb73dbbe6466f09d65bc0b1ce9d1c
Rodrigo Mena <rm.mena86@gmail.com>
2022-01-25 10:40:13 -0300

    Chapter I - Conceitos Importantes - Imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,11 @@
 // Hook -> gancho
 import { useState } from "react";
 
+// Imutabilidade = nunca terá seu valor alterado, sempre irá receber um novo valor
+// usuarios = ["diego", "rafael"]
+// usuarios.push("rodrigo") -> Alterando na memória -> mutação no valor original
+// novoUsuarios = [...usuarios, "rodrigo"] -> Criação de novo espaço na memória -> imutabilidade
+
 export function Counter() {
     // React não monitora mudanças de variáveis para renderizar na tela
     // Estado -> variáveis que são monitoradas os seus estados para renderização, olha só para elas
@@ -8,7 +13,7 @@ export function Counter() {
     const [counter, setCounter] = useState(0);
     
     function increment() {
-        setCounter(counter + 1);
+        setCounter(counter + 1); // Imutabilidade -> counter + 1
     }
 
     return (
